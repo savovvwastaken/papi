e4083ca96c5266efaafe5d030207ee94f15307f7
savovvwastaken <darkkillerabg@gmail.com>
2017-12-12 00:49:26 +0200

    Create pipeWrite.c

---
+++ pipeWrite.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(){
+	char* msg = "TEST_MESSAGE";
+
+	mkfifo("pipe", 0666);
+
+	int pipeFd = open("pipe", O_WRONLY);
+	write(pipeFd, msg, strlen(msg));
+}
